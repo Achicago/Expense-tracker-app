23060f4c57511b3b0e68614d2b17fdea1325952e
achicago <achigonye@gmail.com>
2023-03-16 08:55:44 -0700

    Working on Expense List

--- components/ExpenseOutput/ExpenseItem.js
+++ components/ExpenseOutput/ExpenseItem.js
@@ -1,5 +1,6 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
+// import { getFormattedDate } from "../../util/date";
 
 
 function ExpenseItem({ description, amount, date }) {
@@ -8,10 +9,10 @@ function ExpenseItem({ description, amount, date }) {
             <View style={styles.expenseItem} >
                 <View>
                     <Text style={[styles.textBase, styles.description]} > {description} </Text>
-                    <Text style={styles.textBase} > {date.toString()} </Text>
+                    <Text style={styles.textBase} > {date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()} </Text>
                 </View>
                 <View style={styles.amountContainer} >
-                    <Text style={styles.amount} > {amount}</Text>
+                    <Text style={styles.amount} > {amount.toFixed(2)}</Text>
                 </View>
             </View>
         </Pressable>
@@ -44,11 +45,13 @@ const styles = StyleSheet.create({
     },
     amountContainer: {
         paddingHorizontal: 12,
-        paddingVertical: 4,
+        paddingVertical: 10,
         backgroundColor: 'white',
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 4,
+        minWidth: 80,
+        // height: 38,
     },
     amount: {
         color: GlobalStyles.colors.primary500,
